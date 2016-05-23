60dae1df334c478290da9b182a7a115226606509
Huang Rui <vowstar@gmail.com>
2016-05-24 06:03:27 +0800

    Add cluster mode in emqttd dockerfile

--- library/emqttd/1.0.2/slave/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/opt/emqttd/bin
-sed -i -e "s/^-name\s*.*@.*/-name emqttd@`hostname`/g" /opt/emqttd/etc/vm.args
-/opt/emqttd/bin/emqttd start
-sleep 10
-/opt/emqttd/bin/emqttd_ctl cluster join 'emqttd@'$EMQTTD_MASTER
-tail -f /opt/emqttd/log/*
