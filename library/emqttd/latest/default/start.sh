1cfb418667ce96660d7f2f5f2ecec6edf38fc419
Huang Rui <vowstar@gmail.com>
2016-05-24 21:53:31 +0800

    Update emqttd start method

--- library/emqttd/latest/default/start.sh
+++ library/emqttd/latest/default/start.sh
@@ -5,7 +5,7 @@ SELF_IP=$(cat /etc/hosts | grep ${SELF_HOST} | awk '{print $1}')
 
 sed -i -e "s/^-name\s*.*@.*/-name emqttd@${SELF_IP}/g" /opt/emqttd/etc/vm.args
 
-/opt/emqttd/bin/emqttd start
+/opt/emqttd/bin/emqttd console &
 
 sleep 10
 
@@ -36,4 +36,4 @@ else
 	echo 'cluster master mode'
 fi
 
-tail -f /opt/emqttd/log/*
+fg
