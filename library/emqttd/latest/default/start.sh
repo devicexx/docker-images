f82c420a9b1168532d532a1c72318b0e94f6a981
Huang Rui <vowstar@gmail.com>
2016-05-25 00:22:15 +0800

    Update emqttd start script

--- library/emqttd/latest/default/start.sh
+++ library/emqttd/latest/default/start.sh
@@ -2,16 +2,17 @@
 # Emqttd start script
 SELF_HOST=$(hostname)
 SELF_IP=$(cat /etc/hosts | grep ${SELF_HOST} | awk '{print $1}')
+MASTER_IP=$(cat /etc/hosts | grep '${EMQTTD_MASTER_NAME}' | awk '{print $1}')
 
 sed -i -e "s/^-name\s*.*@.*/-name emqttd@${SELF_IP}/g" /opt/emqttd/etc/vm.args
 
 /opt/emqttd/bin/emqttd start
 
-sleep 10
+echo '['$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")']:emqttd start'
 
-if [ x$EMQTTD_MASTER != x ]
+if [ x$EMQTTD_MASTER_HOST != x ]
 then
-    MASTER_HOST=$EMQTTD_MASTER
+    MASTER_HOST=$EMQTTD_MASTER_HOST
 fi
 
 if [ x$1 != x ]
@@ -33,7 +34,22 @@ then
     echo 'join emqttd@'${MASTER_IP}
 
 else
-	echo 'cluster master mode'
+	if [[ x$MASTER_IP != x && $MASTER_IP != $SELF_IP ]]
+	then
+	    echo 'cluster slave mode'
+	    /opt/emqttd/bin/emqttd_ctl cluster join 'emqttd@'${MASTER_IP}
+	    echo 'join emqttd@'${MASTER_IP}
+	else
+		echo 'cluster master mode'
+	fi
 fi
 
-tail -f /opt/emqttd/log/*
+while [ x$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running') != x ]   
+do  
+	sleep 10
+	echo '['$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")']:emqttd running'
+done
+
+echo '['$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")']:emqttd stop'
+
+cat /opt/emqttd/log/*
