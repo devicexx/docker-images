5eea67bd00c4caf9f3661c49a2d031966b72b226
Huang Rui <vowstar@gmail.com>
2016-06-05 18:38:49 +0800

    Add emqttd 1.1 and 1.1.1

---
+++ library/emqttd/1.1.1/default/start.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# Emqttd start script
+echo 'Emqttd docker image for Device++'
+echo 'Vowstar Co.,Ltd. <support@vowstar.com>'
+echo 'This script is under MIT license'
+
+SELF_HOST=$(hostname)
+SELF_IP=$(cat /etc/hosts | grep ${SELF_HOST} | awk '{print $1}')
+
+sed -i -e "s/^-name\s*.*@.*/-name emqttd@${SELF_IP}/g" /opt/emqttd/etc/vm.args
+
+/opt/emqttd/bin/emqttd start
+
+# wait and ensure emqttd status is running
+while [ x$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') = x ]
+do  
+    sleep 1
+    echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:waiting emqttd'
+done
+
+echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd start'
+
+if [ x$EMQTTD_REMOTE_HOST != x ]
+then
+    REMOTE_HOST=$EMQTTD_REMOTE_HOST
+    echo 'use remote host:'${REMOTE_HOST}
+fi
+
+if [ x$1 != x ]
+then
+    REMOTE_HOST=$1
+    echo 'use remote host:'${REMOTE_HOST}
+fi
+
+echo 'emqttd@'${SELF_IP}
+
+if [ x$REMOTE_HOST != x ]
+then
+    REMOTE_IP=$(cat /etc/hosts | grep ${REMOTE_HOST} | awk '{print $1}')
+    if [ x$REMOTE_IP = x ]
+    then
+        REMOTE_IP=$REMOTE_HOST
+        echo 'local network not have remote host:'${REMOTE_HOST}
+    fi
+    echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:join emqttd@'${REMOTE_IP}
+    /opt/emqttd/bin/emqttd_ctl cluster join 'emqttd@'${REMOTE_IP}
+else
+    if [ x$REMOTE_IP != x ] && [ $REMOTE_IP != $SELF_IP ]
+    then
+        echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:join emqttd@'${REMOTE_IP}
+        /opt/emqttd/bin/emqttd_ctl cluster join 'emqttd@'${REMOTE_IP}
+    fi
+fi
+
+while [ x$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') != x ]
+do  
+    echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd running'
+    DOCKER_IP_LIST=$(cat /etc/hosts |grep -v -E 'localhost|ip6-localnet|ip6-mcastprefix|ip6-allnodes|ip6-allrouters'|awk '{print $1}')
+    CLUSTER_IP_LIST=$(/opt/emqttd/bin/emqttd_ctl cluster status|grep -E -oh '((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])')
+    for TARGET_IP in $DOCKER_IP_LIST
+    do
+        if [ x$(echo $CLUSTER_IP_LIST|grep -oh $TARGET_IP) = x ]
+        then
+            REMOTE_IP=${TARGET_IP}
+            echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:join emqttd@'${REMOTE_IP}
+            /opt/emqttd/bin/emqttd_ctl cluster join 'emqttd@'${REMOTE_IP}
+        fi
+    done
+    sleep 10
+done
+
+tail $(ls /opt/emqttd/log/*)
+
+echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd stop'
